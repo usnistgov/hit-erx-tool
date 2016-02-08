45bc601c2a1b6b2597a56d3579f13d2e5c1fbbe2
Lefort, Maxence C <maxence.lefort@nist.gov>
2016-02-08 15:36:51 -0500

    Update rest controller

--- hit-base-web/src/main/java/gov/nist/hit/erx/web/controller/RestWebServiceController.java
+++ hit-base-web/src/main/java/gov/nist/hit/erx/web/controller/RestWebServiceController.java
@@ -1,18 +1,19 @@
 package gov.nist.hit.erx.web.controller;
 
 import com.google.gson.Gson;
+import gov.nist.hit.MessageIdFinder;
+import gov.nist.hit.MessageTypeFinder;
 import gov.nist.hit.core.domain.TestContext;
-import gov.nist.hit.core.service.exception.MessageParserException;
-import gov.nist.hit.impl.EdiMessageEditor;
 import gov.nist.hit.core.domain.Transaction;
-import gov.nist.hit.core.domain.TransportMessage;
 import gov.nist.hit.core.domain.TransportRequest;
 import gov.nist.hit.core.repo.MessageRepository;
 import gov.nist.hit.core.repo.TestContextRepository;
 import gov.nist.hit.core.service.TransactionService;
 import gov.nist.hit.core.service.TransportMessageService;
+import gov.nist.hit.core.service.exception.MessageParserException;
 import gov.nist.hit.core.transport.exception.TransportClientException;
 import gov.nist.hit.erx.ws.client.Message;
+import gov.nist.hit.impl.EdiMessageEditor;
 import gov.nist.hit.impl.XMLMessageEditor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +21,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by mcl1 on 1/13/16.
@@ -48,6 +49,7 @@ public class RestWebServiceController {
     @Autowired
     protected TestContextRepository testContextRepository;
 
+
     @Transactional()
     @RequestMapping(value = "/message", method = RequestMethod.POST)
     public String message(@RequestBody TransportRequest request) throws TransportClientException, MessageParserException {
@@ -55,7 +57,6 @@ public class RestWebServiceController {
         Gson gson = new Gson();
         String responseMessage = "";
         String jsonRequest = gson.toJson(request);
-        //{"config":{"username":"vendor_1_396","password":"vendor_1_396"},"message":"UNA:+./*"}
         Message received = gson.fromJson(jsonRequest,Message.class);
         logger.info("Message received : " + jsonRequest);
         //TODO modify the response message
@@ -71,15 +72,20 @@ public class RestWebServiceController {
              message = messageRepository.getOne(messageId);
             if (message != null) {
                 TestContext testContext = testContextRepository.findOneByMessageId(messageId);
+                MessageTypeFinder messageTypeFinder = MessageTypeFinder.getInstance();
                 if(testContext.getFormat().toLowerCase().equals("edi")) {
                     EdiMessageEditor ediMessageEditor = new EdiMessageEditor();
                     try {
+                        String receivedMessageType = messageTypeFinder.findEdiMessageType(received.getMessage());
                         HashMap<String, String> replaceTokens = new HashMap<>();
-                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
-                        simpleDateFormat.applyPattern("yyyymmdd");
-                        replaceTokens.put("UIB-080-01", simpleDateFormat.format(new Date()));
-                        simpleDateFormat.applyPattern("HHmmss");
-                        replaceTokens.put("UIB-080-02", simpleDateFormat.format(new Date()));
+                        if(receivedMessageType.equals("chgres")||receivedMessageType.equals("refres")){
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+                            simpleDateFormat.applyPattern("yyyymmdd");
+                            replaceTokens.put("UIB-080-01", simpleDateFormat.format(new Date()));
+                            simpleDateFormat.applyPattern("HHmmss");
+                            replaceTokens.put("UIB-080-02", simpleDateFormat.format(new Date()));
+                        }
+                        replaceTokens.put("UIH-020",receivedMessageType);
                         responseMessage = ediMessageEditor.replaceInMessage(message, replaceTokens, testContext);
                         logger.info("Generated response message : " + responseMessage);
                     } catch (Exception e) {
@@ -88,12 +94,13 @@ public class RestWebServiceController {
                     }
                     transaction.setOutgoing(responseMessage);
                 } else if(testContext.getFormat().toLowerCase().equals("xml")){
-                    XMLMessageEditor xmlMessageEditor = new XMLMessageEditor();
-                    HashMap<String, String> replaceTokens = new HashMap<>();
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
-                    simpleDateFormat.applyPattern("yyyy-MM-dd'T'HH:mm:ss");
-                    replaceTokens.put("SentTime", simpleDateFormat.format(new Date()));
                     try {
+                        String receivedMessageType = messageTypeFinder.findXmlMessageType(received.getMessage());
+                        XMLMessageEditor xmlMessageEditor = new XMLMessageEditor();
+                        HashMap<String, String> replaceTokens = new HashMap<>();
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+                        simpleDateFormat.applyPattern("yyyy-MM-dd'T'HH:mm:ss");
+                        replaceTokens.put("SentTime", simpleDateFormat.format(new Date()));
                         responseMessage = xmlMessageEditor.replaceInMessage(message,replaceTokens,testContext);
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -110,6 +117,8 @@ public class RestWebServiceController {
         transactionService.save(transaction);
         return responseMessage;
     }
+
+
 
     @Transactional()
     @RequestMapping(value = "/test", method = RequestMethod.GET)
